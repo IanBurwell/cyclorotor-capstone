23efa7c7b32f6fef853876e6fd1b0eb05642cf2e
Matthew Tortolani <64276642+MTorts13@users.noreply.github.com>
2022-10-04 16:50:34 -0400

    redboard servo code
    implements code for servo control.  asks for input (0 to 360) and gives it to the servo

--- Redboard_test/redboard_test_9-30-2022/redboard_test_9-30-2022.ino
+++ Redboard_test/redboard_test_9-30-2022/redboard_test_9-30-2022.ino
@@ -1,30 +1,42 @@
 #include <Servo.h>
 
-#define MOTOR1 9
+//pin for servo 1
+#define MOTOR1 3 
+          
+Servo servo1; //variable of Servo type named servo1
 
-#define SERIAL_TX2 A02
-#define SERIAL_RX2 A03
+//Functions
+float degree2ms(float degrees)  //Convert Degrees to microseconds, which is read by servos
+{
+  return 500 + degrees * 200.0/18;
+}
 
-Servo esc, servo1, servo2;
+float gear_ratio(float circle)  //converts input angle (which can be 0 to 360) to the angle of motion of the servo (currently 180 deg.)
+{
+return  circle/2;
+}
+
 
 void setup(){
   Serial.begin(9600);
 
   servo1.attach(MOTOR1);
-
 }
 
 void loop(){
+//Serial.print("Please input an angle in degrees, only put in the number")
 
   if(Serial.available()){
-    char c = Serial.read();    
-    if(c == '2'){
-      esc.writeMicroseconds(1500);
-      servo1.writeMicroseconds(1500);
-      servo2.writeMicroseconds(1500);
-    }
+    String input_angle = Serial.readStringUntil('\n');   
+    float angle = input_angle.toFloat();
+    angle = gear_ratio(angle);
+    angle = degree2ms(angle);
 
-    Serial.println("The input was " + c);
-    Serial.println(c);
+    servo1.writeMicroseconds(angle);
+      
+    Serial.print("The servo is mvoving towards ");
+    Serial.print(input_angle);
+    Serial.print(". \n");
+
   }
 }
