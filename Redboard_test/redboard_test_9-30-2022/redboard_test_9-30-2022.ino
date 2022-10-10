fa753f288fbb0df11b4c811b335863dc463a189f
Matthew Tortolani <64276642+MTorts13@users.noreply.github.com>
2022-10-10 13:31:22 -0400

    Update redboard_test_9-30-2022.ino
    Adds linear actuator and ESC code.

--- Redboard_test/redboard_test_9-30-2022/redboard_test_9-30-2022.ino
+++ Redboard_test/redboard_test_9-30-2022/redboard_test_9-30-2022.ino
@@ -1,46 +1,68 @@
 #include <Servo.h>
 
-//pin for servo 1
+//pins for servo, lin actuator, ESC
 #define MOTOR1 3 
+#define MOTOR2 4
+#define MOTOR3 9
           
 Servo servo1; //variable of Servo type named servo1
+Servo lin_act;
+Servo ESC;
 
 //Functions
 float degree2ms(float degrees){  //Convert Degrees to microseconds, which is read by servos
-  return 500 + degrees * 200.0/18;
+  return 500 + (degrees * 200.0/18);
 }
-
 
 float gear_ratio(float circle){  //converts input angle (which can be 0 to 360) to the angle of motion of the servo (currently 180 deg.)
   return  circle/2;
 }
 
+float eccen2ms(float eccen){  //Convert eccen to microseconds, which is read by the linear actuator
+  return 1000 + (eccen * 1000.0/30);
+}
+
+float esc2ms(float esc){ //Convert esc to microseconds, which is read by the ESC
+  return 1000 + (esc *10);
+}
 
 void setup(){
   Serial.begin(9600);
 
   servo1.attach(MOTOR1);
-  servo1.
+  lin_act.attach(MOTOR2);
+  ESC.attach(MOTOR3);
 
   // Wait for serial port to connect
   while(!Serial);
-  Serial.println("Ready");
+  Serial.println("Ready. \nSet Serial Monitor to 'No Line Ending.' \nInput angle,eccentricity,motorpower (degrees,mm,%)");
 
 }
 
 void loop(){
-//Serial.print("Please input an angle in degrees, only put in the number")
 
   if(Serial.available()){
-    float angle = Serial.parseFloat();
-    angle = gear_ratio(angle);
+    float input_angle = Serial.parseFloat();
+    float angle = gear_ratio(input_angle);
     angle = degree2ms(angle);
 
+    float input_eccen = Serial.parseFloat();
+    float eccen = eccen2ms(input_eccen);
+
+    float input_esc = Serial.parseFloat();
+    float esc = esc2ms(input_esc);
+
     servo1.writeMicroseconds(angle);
+    lin_act.writeMicroseconds(eccen);
+    ESC.writeMicroseconds(esc);
       
     Serial.print("The servo is moving towards ");
     Serial.print(angle);
-    Serial.print(". \n");
-
+    Serial.print(" degrees. \nThe lin. actuator is moving toward ");
+    Serial.print(eccen);
+    Serial.print(" mm. \nThe motor is at ");
+    Serial.print(esc);
+    Serial.print(" %. \n");
+    
   }
 }
